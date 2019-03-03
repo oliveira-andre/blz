d449d00d628cfa9be5aa9fb73c31c7675728827a
Andre Oliveira <desmonplays@gmail.com>
2019-03-03 19:27:24 -0400

    (new feature) adding create new and params array

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -6,6 +6,19 @@ class ServicesController < ApplicationController
     @services = @establishment.services
   end
 
+  def new
+    @service = @establishment.services.build
+  end
+
+  def create
+    @service = @establishment.services.build(service_params)
+    if @service.save
+      redirect_to establishment_services_path, notice: "Salvo com sucesso"
+    else
+      render 'new'
+    end
+  end
+
   private
 
   def load_establishment
@@ -13,6 +26,8 @@ class ServicesController < ApplicationController
   end
 
   def service_params
-    require(:service).permit(:photo)
+    params.require(:service).permit(:title, :description, :amount, :duration,
+                                    :category_id, :local_type, :status,
+                                    photos: [])
   end
 end
