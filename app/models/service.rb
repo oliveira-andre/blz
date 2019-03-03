902fe4413d57e610682c738be23cd8c19db85d6f
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-03-03 09:26:09 -0300

    fix validation in service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,12 +1,11 @@
 class Service < ApplicationRecord
   belongs_to :category
-  belongs_to :estableshment
+  belongs_to :estableshment, required: false
 
   validates :title, presence: true
   validates :description, presence: true
   validates :amount, presence: true
   validates :duration, presence: true
-  validates :timetable, presence: true
 
   validates :title, uniqueness: { scope: :establishment_id }
 end
