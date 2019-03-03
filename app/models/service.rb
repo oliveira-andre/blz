21d1b50ff55b507efc7f6c111cc36c6d6e06be83
Andre Oliveira <desmonplays@gmail.com>
2019-03-03 19:27:51 -0400

    (new feature) adding the active storage to service

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,13 +1,18 @@
 class Service < ApplicationRecord
+  enum status: %i[approved recused awaiting_avaliation archived]
+  enum local_type: %i[home establishment]
+
   has_many_attached :photos
 
   belongs_to :category
-  belongs_to :estableshment, required: false
+  belongs_to :establishment
 
   validates :title, presence: true
   validates :description, presence: true
   validates :amount, presence: true
   validates :duration, presence: true
+  validates :local_type, presence: true
+  validates :status, presence: true
 
   validates :title, uniqueness: { scope: :establishment_id }
 end
