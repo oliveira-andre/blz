61ede2d444f1b6651bae0895c52601abaf638e99
Andre Oliveira <desmonplays@gmail.com>
2019-03-04 15:06:26 -0400

    (new feature) when change the image, preview it to user

--- app/assets/javascripts/preview_image.js
+++ app/assets/javascripts/preview_image.js
@@ -17,4 +17,15 @@ $(document).on("turbolinks:load", function () {
       reader.readAsDataURL(files.item(i));
     }
   });
+
+  $('#user-photo-add').on('change', function () {
+    var files = this.files;
+    if (!files) return;
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      var template_string = `<img class="is-rounded" src="${event.target.result}">`;
+      $("#temporary_image").html(template_string);
+    }
+    reader.readAsDataURL(files.item(0));
+  });
 });
