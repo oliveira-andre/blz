b0efa484407a3897d33b7ead03c08cabbfd21a34
Andre Oliveira <desmonplays@gmail.com>
2019-03-03 21:57:59 -0400

    (new feature) adding the name to permit devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError do
     respond_to do |format|
-      format.html { redirect_to root_path, flash: { error: 'Não autorizado' } }
+      format.html {redirect_to root_path, flash: {error: 'Não autorizado'}}
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
