ca6a22db3f35d11198a0d3578b6971696f7f18ce
Andre Oliveira <desmonplays@gmail.com>
2019-03-03 20:05:40 -0400

    (new feature) updating now the service

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,6 @@
 class ServicesController < ApplicationController
-  before_action :load_establishment
+  before_action :load_establishment, only: %i[index new create]
+  before_action :load_services, only: %i[edit update]
 
   def index
     authorize @establishment, policy_class: ServicePolicy
@@ -7,15 +8,30 @@ class ServicesController < ApplicationController
   end
 
   def new
+    authorize @establishment, policy_class: ServicePolicy
     @service = @establishment.services.build
   end
 
   def create
     @service = @establishment.services.build(service_params)
+    authorize @service
     if @service.save
-      redirect_to establishment_services_path, notice: "Salvo com sucesso"
+      redirect_to establishment_services_path, notice: 'Salvo com sucesso'
     else
       render 'new'
+    end
+  end
+
+  def edit
+    authorize @service
+  end
+
+  def update
+    authorize @service
+    if @service.update(service_params)
+      redirect_to establishment_services_path, notice: 'Atualizado com sucesso'
+    else
+      render 'edit'
     end
   end
 
@@ -23,6 +39,10 @@ class ServicesController < ApplicationController
 
   def load_establishment
     @establishment = Establishment.find(params[:establishment_id])
+  end
+
+  def load_services
+    @service = Service.find(params[:id])
   end
 
   def service_params
