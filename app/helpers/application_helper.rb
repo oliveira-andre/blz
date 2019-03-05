b130a2efa181d10248426f4d3bb6fc46a34ef88a
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-03-05 17:03:14 -0300

    create helpers to format date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def week_day(date_time)
+    I18n.l(date_time, format: :week_day)
+  end
+
+  def time(date_time)
+    I18n.l(date_time, format: :time)
+  end
+
+  def day_month(date_time)
+    I18n.l(date_time, format: :day_month)
+  end
 end
