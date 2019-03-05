9c12a48c134943c068774355d8b693e3c28a2719
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-03-05 17:45:10 -0300

    create method to show full address

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,8 @@ class Address < ApplicationRecord
   validates :street, presence: true
   validates :number, presence: true
   validates :neighborhood, presence: true
+
+  def full
+    "#{street}, #{number}, #{neighborhood} - #{city} / #{state.upcase[0..1]}"
+  end
 end
