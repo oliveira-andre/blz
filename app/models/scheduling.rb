28f56b043095cba321aa137a7142cf658a3c4a1b
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-03-05 22:14:26 -0300

    create scheduling model

---
+++ app/models/scheduling.rb
@@ -0,0 +1,45 @@
+class Scheduling < ApplicationRecord
+  enum status: %i[in_payment awaiting_service finished canceled]
+
+  belongs_to :user, required: false
+  belongs_to :service
+
+  validates :status, presence: true
+  validates :begin_time, presence: true
+  validates :end_time, presence: true
+
+  validates_with DatePastValidator
+
+  validate :uniqueness_time_by_user
+  validate :uniqueness_time_by_establishment
+
+  private
+
+  def uniqueness_time_by_establishment
+    return if service_id.nil?
+
+    current_establishment = service.establishment
+    services_ids_by_establishment = current_establishment.services.ids
+    scheduling_ids = Scheduling.where(
+      end_time: (begin_time..end_time),
+      service_id: services_ids_by_establishment
+    ).ids
+
+    return if scheduling_ids.empty?
+
+    @errors.add(:begin_time, 'já esta ocupado para esse salão/profissional')
+  end
+
+  def uniqueness_time_by_user
+    return if user_id.nil?
+
+    scheduling_ids = Scheduling.where(
+      end_time: (begin_time..end_time),
+      user_id: user_id
+    ).ids
+
+    return if scheduling_ids.empty?
+
+    @errors.add(:begin_time, 'já esta ocupado na sua agenda')
+  end
+end
