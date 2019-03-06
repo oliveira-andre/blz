c1720f71cc1c32cbd1856a96beab9006d79c7445
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-03-05 22:13:17 -0300

    add flash message in scheduling controller

--- app/controllers/scheduling_controller.rb
+++ app/controllers/scheduling_controller.rb
@@ -1,14 +1,14 @@
 class SchedulingController < ApplicationController
   def create
     @scheduling = Scheduling.new scheduling_params
+    @scheduling.end_time = @scheduling.begin_time +
+                           @scheduling.service.duration.minutes
     authorize @scheduling
 
     if @scheduling.save
       redirect_to user_dashboard_path(id: @scheduling.user.id)
     else
-      @scheduling.errors.full_messages.each do |message|
-        flash[:error] = message
-      end
+      add_flash_message
       redirect_to service_path(id: @scheduling.service.id)
     end
   end
@@ -16,6 +16,12 @@ class SchedulingController < ApplicationController
   private
 
   def scheduling_params
-    params.permit(:service_id, :timetable).merge(user_id: current_user.id)
+    params.permit(:service_id, :begin_time).merge(user_id: current_user.id)
+  end
+
+  def add_flash_message
+    @scheduling.errors.full_messages.each do |message|
+      flash[:error] = message
+    end
   end
 end
