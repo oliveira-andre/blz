01d625dfa96a7c3759363ed9ea3108302874569c
Andre Oliveira <desmonplays@gmail.com>
2019-03-05 21:09:54 -0400

    (fix bug) deleting the two points into hours

--- app/controllers/office_hours_controller.rb
+++ app/controllers/office_hours_controller.rb
@@ -1,7 +1,9 @@
 class OfficeHoursController < ApplicationController
+  before_action :clean_hours, only: :create
+
   def create
     @office_hour = OfficeHour.new(office_hours_params)
-    if @office_hour.save
+    if @office_hour.save!
       redirect_to edit_establishment_service_path(@office_hour.service.establishment.id, @office_hour.service)
     else
       flash[:error] = 'Campos inválidos, verifique se estão corretos'
@@ -18,5 +20,10 @@ class OfficeHoursController < ApplicationController
 
   def office_hours_params
     params.permit(:hour_begin, :hour_end, :week_day, :service_id)
+  end
+
+  def clean_hours
+    params[:hour_begin].delete!(':')
+    params[:hour_end].delete!(':')
   end
 end
