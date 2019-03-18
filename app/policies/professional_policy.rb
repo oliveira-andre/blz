28b14b6fc2902caff1a74b6425a4f8d6c5114491
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-03-18 19:49:05 -0400

    create professional policy

---
+++ app/policies/professional_policy.rb
@@ -0,0 +1,17 @@
+class ProfessionalPolicy < ApplicationPolicy
+  def new?
+    record.establishment.user == user
+  end
+
+  def create?
+    record.establishment.user == user
+  end
+
+  def edit?
+    record.establishment.user == user
+  end
+
+  def update?
+    record.establishment.user == user
+  end
+end
