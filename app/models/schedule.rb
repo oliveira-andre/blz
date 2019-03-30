67a1a8490f2f75d60c94f73bc950471309664f1c
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-03-30 11:44:45 -0400

    create method rebuild in schedule model

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,7 @@
 class Schedule < ApplicationRecord
   belongs_to :professional_service
+
+  def self.rebuild(professional_service)
+    BuildScheduleJob.perform_later(professional_service)
+  end
 end
