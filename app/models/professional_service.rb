0233b0c7266ad993421e0cbd505eddfb86aaf54f
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-03-30 11:42:54 -0400

    create method to return table schedule in professional service

--- app/models/professional_service.rb
+++ app/models/professional_service.rb
@@ -3,4 +3,13 @@ class ProfessionalService < ApplicationRecord
   belongs_to :service
 
   has_many :schedules
+
+  def table_schedule
+    return [] if schedules.empty?
+
+    schedules.to_a.map! do |schedule|
+      dates = schedules.where(date: schedule.date.all_day).pluck(:date)
+      { I18n.l(schedule.date, format: :day_month) => dates }
+    end.uniq!
+  end
 end
