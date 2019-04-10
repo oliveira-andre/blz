7c581418487b54307b254445aa62cb22bc358cfb
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-04-09 23:44:36 -0400

    add enum, relationships and moip update method to establishment model

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -1,25 +1,20 @@
 class Establishment < ApplicationRecord
-  belongs_to :user
+  enum status: %i[authorization analyze approved disapproved canceled]
 
-  validates :cpf_cnpj, presence: true
   validates :name, presence: true
-  validates :email, presence: true
-  validates :phone, presence: true
   validates :timetable, presence: true
-
   validates :user_id, uniqueness: true
+  validates :address, presence: true
+
+  belongs_to :user
 
   has_many :services
   has_many :professionals
-
-  has_one :address
+  has_one :address, dependent: :destroy
   has_one_attached :photo
 
-  accepts_nested_attributes_for :address, allow_destroy: true,
-                                          reject_if: :all_blank
-
-  accepts_nested_attributes_for :user, allow_destroy: true,
-                                       reject_if: :all_blank
+  accepts_nested_attributes_for :address, allow_destroy: true
+  accepts_nested_attributes_for :user, allow_destroy: true
 
   def scheduling
     professional_services_ids = ProfessionalService.where(
@@ -27,5 +22,14 @@ class Establishment < ApplicationRecord
       service_id: services.ids
     ).uniq
     ::Scheduling.where(professional_service_id: professional_services_ids)
+  end
+
+  def moip_update(moip_response)
+    update(
+      moip_access_token: moip_response.access_token,
+      moip_refresh_token: moip_response.refresh_token,
+      moip_set_password_link: nil,
+      status: :analyze
+    )
   end
 end
