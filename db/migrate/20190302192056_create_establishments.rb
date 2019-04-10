fe254bde87ece127a8ed9476d75601a29c74b625
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-04-09 23:29:09 -0400

    add moip fields to establishment

--- db/migrate/20190302192056_create_establishments.rb
+++ db/migrate/20190302192056_create_establishments.rb
@@ -1,12 +1,15 @@
 class CreateEstablishments < ActiveRecord::Migration[5.2]
   def change
     create_table :establishments do |t|
-      t.string :cpf_cnpj, null: false
       t.string :name, null: false
-      t.string :email, null: false
-      t.string :phone, null: false
       t.string :timetable, null: false
+      t.integer :status, default: 0
       t.references :user, foreign_key: true
+
+      t.string :moip_account_id
+      t.string :moip_access_token
+      t.string :moip_refresh_token
+      t.string :moip_set_password_link
 
       t.timestamps
     end
