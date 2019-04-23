d1ba61a49a7886d502890723e58886ac369658f4
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-04-23 16:38:59 -0400

    add filter to list only paids scheduling to establishments

--- app/controllers/establishments_dashboard_controller.rb
+++ app/controllers/establishments_dashboard_controller.rb
@@ -2,7 +2,7 @@ class EstablishmentsDashboardController < ApplicationController
   def index
     @establishment = Establishment.find(params[:id])
     authorize @establishment
-    @scheduling = @establishment.scheduling.awaiting_service
+    @scheduling = @establishment.scheduling.paid
     @scheduling_history = @establishment.scheduling.history
   end
 end
