ecb590c228554be121e5692979a02da72ec89036
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-04-23 15:59:35 -0400

    create service to generate access token in moip

--- app/services/moip/account/generate_access_token_service.rb
+++ app/services/moip/account/generate_access_token_service.rb
@@ -4,13 +4,16 @@ module Moip
       class << self
         def execute(code)
           moip_app = Moip::APIService.execute
-          moip_app.connect.authorize(
+          response = moip_app.connect.authorize(
             client_id: credentials[:app_id],
             client_secret: credentials[:app_secret],
             code: code,
             redirect_uri: credentials[:app_uri],
             grant_type: :authorization_code
           )
+          return if response.try(:code) == 400
+
+          response
         end
 
         private
