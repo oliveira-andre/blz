eeb8e8de086919a207cf6ebdb9bce11cdee496b0
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-04-23 15:49:19 -0400

    make address polymorphic

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,19 @@
 class Address < ApplicationRecord
-  belongs_to :establishment
-
   validates :street, presence: true
   validates :number, presence: true
   validates :neighborhood, presence: true
 
+  belongs_to :addressable, polymorphic: true
+
   def full
     "#{street}, #{number}, #{neighborhood} - #{city} / #{state.upcase[0..1]}"
+  end
+
+  def self.update_or_create(addressable, params)
+    if addressable.address
+      addressable.address.update!(params)
+    else
+      Address.create!(address_params)
+    end
   end
 end
--- db/migrate/20190302192935_create_addresses.rb
+++ db/migrate/20190302192935_create_addresses.rb
@@ -8,7 +8,7 @@ class CreateAddresses < ActiveRecord::Migration[5.2]
       t.string :state, default: 'RO'
       t.string :country, default: 'BRA'
       t.string :zipcode
-      t.references :establishment, foreign_key: true
+      t.references :addressable, polymorphic: true, index: true
 
       t.timestamps
     end
