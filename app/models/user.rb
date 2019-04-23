fe9d5c8e51d0012a45e58763d61ede29420c4c4d
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-04-23 16:13:10 -0400

    add validation to model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable
 
   validates :name, presence: true
-  validates :birth_date, presence: true
+  validates :birth_date, presence: true, if: :establishment_or_update?
   validates :terms_acceptation, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :cpf, presence: true, uniqueness: true
-  validates_cpf :cpf
+  validates :cpf, presence: true,
+                  uniqueness: true,
+                  if: :establishment_or_update?
+  validates_cpf :cpf, if: :establishment_or_update?
 
   has_many :scheduling
   has_one :establishment
+  has_one :address, as: :addressable
   has_many :scheduling
+
+  accepts_nested_attributes_for :address, allow_destroy: true
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -19,5 +24,9 @@ class User < ApplicationRecord
       user.name = auth.info.name
       user.password = Devise.friendly_token[0, 20]
     end
+  end
+
+  def establishment_or_update?
+    !establishment.nil? || !id.nil?
   end
 end
