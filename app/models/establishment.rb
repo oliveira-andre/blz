968000828d988ee0bbff1a1d668e1e9665a53885
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-04-23 16:10:33 -0400

    add callback after save to establishment to create moip account

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -4,17 +4,18 @@ class Establishment < ApplicationRecord
   validates :name, presence: true
   validates :timetable, presence: true
   validates :user_id, uniqueness: true
-  validates :address, presence: true
 
   belongs_to :user
 
   has_many :services
   has_many :professionals
-  has_one :address, dependent: :destroy
+  has_one :address, as: :addressable
   has_one_attached :photo
 
   accepts_nested_attributes_for :address, allow_destroy: true
   accepts_nested_attributes_for :user, allow_destroy: true
+
+  after_create :create_moip_account, unless: :exist_moip_account?
 
   def scheduling
     professional_services_ids = ProfessionalService.where(
@@ -26,10 +27,24 @@ class Establishment < ApplicationRecord
 
   def moip_update(moip_response)
     update(
+      moip_account_id: moip_response.moip_account.id,
       moip_access_token: moip_response.access_token,
       moip_refresh_token: moip_response.refresh_token,
       moip_set_password_link: nil,
       status: :analyze
     )
+  end
+
+  private
+
+  def exist_moip_account?
+    Moip::Account::ExistService.execute(
+      tax_document: user.cpf,
+      email: user.email
+    )
+  end
+
+  def create_moip_account
+    Moip::Account::CreateService.execute self
   end
 end
