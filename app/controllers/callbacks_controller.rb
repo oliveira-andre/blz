0131d424e3bd22fc86a5d7d1a36c34eb65280a0b
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-04-23 16:36:18 -0400

    create callback controller to authorization moip

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,8 +1,14 @@
 class CallbacksController < ApplicationController
   def index
     response = Moip::Account::GenerateAccessTokenService.execute(params[:code])
-    current_user.establishment.moip_update(response)
-    redirect_to establishments_dashboard_path(current_user.establishment),
-                notice: 'Parabéns sua conta foi autorizada'
+    if response.nil?
+      redirect_to establishments_dashboard_path(current_user.establishment),
+                  flash: { error: 'Código de autorização não encontrado' }
+    else
+      current_user.establishment.moip_update(response)
+      redirect_to establishments_dashboard_path(current_user.establishment),
+                  notice: 'Parabéns sua conta foi autorizada'
+    end
   end
+
 end
