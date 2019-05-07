6c9f4e2fdefc9141c47208c430cd3a37a008e568
Gabriel Flauzino <flaugabriel@gmail.com>
2019-05-07 02:13:54 -0400

    add file plugin

--- app/assets/javascripts/preview_image.js
+++ app/assets/javascripts/preview_image.js
@@ -1,3 +1,29 @@
 $(document).on("turbolinks:load", function () {
+    $('#drag-and-drop-zone').dmUploader({
+        url: '/demo/java-script/upload',
+        maxFileSize: 3000000, // 3 Megs max
+        allowedTypes: 'image/*',
+        extFilter: ['jpg', 'jpeg', 'png', 'gif'],
+        // ...
+        onNewFile: function (id, file) {
+            //...
+            if (typeof FileReader !== 'undefined') {
+                var reader = new FileReader();
+                var img = $('<img />');
 
+                reader.onload = function (e) {
+                    img.attr('src', e.target.result);
+                }
+                /* ToDo: do something with the img! */
+                reader.readAsDataURL(file);
+            }
+        },
+        onFileTypeError: function (file) {
+            // ...
+        },
+        onFileTypeError: function (file) {
+            // ...
+        }
+        // ...
+    });
 });
