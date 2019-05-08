02f6a69dc30474352c43fc48586c0c883b02dbf2
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-05-08 10:34:31 -0400

    fix search query filter

--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -1,6 +1,46 @@
 $(document).on("turbolinks:load", function () {
+  runHomePage();
+});
 
-  //OPEN BULMA CALENDAR
+function runHomePage() {
+  // RETURN IF NOT BE HOME PAGE
+  if(!$('.pages-home')) return;
+
+  // BULMA CALENDAR
+  launchBulmaCalendar();
+
+  // SCROLL TO SHOW FOOTER IN MOBILE
+  scrollFooterShow();
+
+  // SEARCH AND FILTERS
+  listenerSearchAndFilter();
+}
+
+function listenerSearchAndFilter() {
+  var eventTimeout = null;
+  $('#query').keyup(function(e) {
+    var query = $('#query').val();
+    if(!(query && query.length >= 3)) return;
+
+    if(eventTimeout) {
+      clearTimeout(eventTimeout);
+      eventTimeout = null;
+    }
+    eventTimeout = setTimeout(submitSearchAndFilters, 600);
+  });
+}
+
+function submitSearchAndFilters() {
+  $('#search-submit').click();
+  toggleLoading();
+}
+
+function toggleLoading() {
+  $('.field span img').toggleClass('is-hidden');
+  $('.field .control').toggleClass('is-loading');
+}
+
+function launchBulmaCalendar() {
   bulmaCalendar.attach('#home_filter_date', {
     minDate: new Date(),
     lang: 'pt',
@@ -12,8 +52,9 @@ $(document).on("turbolinks:load", function () {
     enableYearSwitch: false,
     enableMonthSwitch: false
   });
+}
 
-  //SCROLL TO SHOW FOOTER IN MOBILE
+function scrollFooterShow() {
   $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
@@ -23,4 +64,4 @@ $(document).on("turbolinks:load", function () {
       $('footer.mobile').addClass('is-hidden')
     }
   });
-});
+}
