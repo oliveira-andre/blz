824de98658fcd6b8daa94c8d711a7607cd98c41c
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-05-09 18:20:51 -0400

    create thumbnail helper to models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,4 +4,16 @@ class ApplicationRecord < ActiveRecord::Base
   def t_status
     I18n.t("activerecord.enums.#{model_name.name.downcase}.#{status}")
   end
+
+  def thumbnail(photo, resize = :medium)
+    return if photo.nil?
+
+    resizes = {
+      avatar: '64x64!',
+      small: '320x240!',
+      medium: '640x480!',
+      large: '1280x960!'
+    }
+    photo.variant(resize: resizes[resize]).processed
+  end
 end
