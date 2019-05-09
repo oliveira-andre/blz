93af802d2d120d5de4884a6df5353577651b7193
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-05-09 18:21:15 -0400

    refactor service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,50 +1,32 @@
 class Service < ApplicationRecord
   enum status: %i[approved recused awaiting_avaliation archived]
   enum local_type: %i[home establishment]
+
   has_many_attached :photos
+
   belongs_to :category
   belongs_to :establishment
+
   has_many :professional_services
   has_many :professionals, through: :professional_services
+
   validates :title, presence: true
+  validates :title, uniqueness: { scope: :establishment_id }
   validates :description, presence: true
   validates :amount, presence: true
   validates :duration, presence: true
   validates :local_type, presence: true
   validates :status, :photos, presence: true
-  validate :photos_type
-  validates :title, uniqueness:{scope: :establishment_id}
+
+  validate :photo_type
+  validate :limit_number_photos
 
   def professionals_to_link
-    professionals_ids = ProfessionalService.where(service_id: id).pluck(:professional_id)
-    Professional.where(establishment_id: establishment_id).where.not(id: professionals_ids)
-  end
+    professionals_ids = ProfessionalService.where(service_id: id)
+                                           .pluck(:professional_id)
 
-  def thumbmail_smal input
-    return self.photos[input].variant(resize: "320x240!").processed
-  end
-
-  def thumbmail_full input
-    return self.photos[input].variant(resize: "1280x960!").processed
-  end
-
-  private
-
-  def photos_type
-    object_array = []
-    photos.each do |p|
-      object_array.push(p)
-    end
-
-    if object_array.length > 5
-      errors.add(:atenção!, 'Você deve inserir no máximo 5 fotos')
-    end
-
-    photos.each do |photo|
-      if !photo.content_type.in?(%('image/jpeg image/png))
-        errors.add(:"atenção!", 'Apenas fotos com extenção JPEG e PNG')
-      end
-    end
+    Professional.where(establishment_id: establishment_id)
+                .where.not(id: professionals_ids)
   end
 
   def update_and_rebuild_schedule(params)
@@ -56,6 +38,19 @@ class Service < ApplicationRecord
   end
 
   private
+
+  def limit_number_photos
+    errors.add(:photos, '5 no máximo.') if photos.count > 5
+  end
+
+  def photo_type
+    photos.each do |photo|
+      next if photo.content_type.in?(%(image/jpeg image/png))
+
+      errors.add(:photos, 'com tipo não permitido.')
+      break
+    end
+  end
 
   def rebuild_schedule
     professional_services.each { |p_s| Schedule.rebuild(p_s) }
