61db1f39ae06f5bd92c7a174d1cd3d2483ff6637
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-05-09 09:21:29 -0400

    improve and adjust script to filters with categories, query string, date and local

--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -2,12 +2,12 @@ $(document).on("turbolinks:load", function () {
   runHomePage();
 });
 
-function runHomePage() {
+function runHomePage(filter_date) {
   // RETURN IF NOT BE HOME PAGE
   if(!$('.pages-home')) return;
 
   // BULMA CALENDAR
-  launchBulmaCalendar();
+  launchBulmaCalendar(filter_date);
 
   // SCROLL TO SHOW FOOTER IN MOBILE
   scrollFooterShow();
@@ -18,21 +18,49 @@ function runHomePage() {
 
 function listenerSearchAndFilter() {
   var eventTimeout = null;
+
   $('#query').keyup(function(e) {
-    var query = $('#query').val();
-    if(!(query && query.length >= 3)) return;
+    if(eventTimeout) {
+      clearTimeout(eventTimeout);
+      eventTimeout = null;
+    }
+    eventTimeout = setTimeout(function() {
+      $('#search-submit').click();
+      toggleLoading();
+    }, 600);
+  });
+
+  $('input[name="local_type[]"], input[name="categories_ids[]"]').change(function(e){
+    if(e.target.name == "categories_ids[]") {
+      e.target.parentElement.classList.toggle('is-active');
+      $(e.target.parentElement.children[1]).toggleClass('button is-loading is-size-3')
+      e.target.parentElement.children[1].children[0].classList.toggle('is-hidden')
+    }
 
     if(eventTimeout) {
       clearTimeout(eventTimeout);
       eventTimeout = null;
     }
-    eventTimeout = setTimeout(submitSearchAndFilters, 600);
-  });
-}
 
-function submitSearchAndFilters() {
-  $('#search-submit').click();
-  toggleLoading();
+    eventTimeout = setTimeout(function() {
+      $('#filters-submit').click();
+    }, 600);
+  });
+
+  const datepicker = document.querySelector('#home_filter_date');
+
+  if (datepicker) {
+    datepicker.bulmaCalendar.on('select', function() {
+      if(eventTimeout) {
+        clearTimeout(eventTimeout);
+        eventTimeout = null;
+      }
+
+      eventTimeout = setTimeout(function() {
+        $('#filters-submit').click();
+      }, 600);
+    });
+  }
 }
 
 function toggleLoading() {
@@ -40,10 +68,11 @@ function toggleLoading() {
   $('.field .control').toggleClass('is-loading');
 }
 
-function launchBulmaCalendar() {
+function launchBulmaCalendar(filter_date) {
   bulmaCalendar.attach('#home_filter_date', {
     minDate: new Date(),
-    lang: 'pt',
+    lang: 'pt-BR',
+    startDate: dateFormartPTBR(filter_date),
     dateFormat: 'DD/MM/YYYY',
     color: '$primary-color',
     cancelLabel: 'Cancelar',
@@ -64,4 +93,19 @@ function scrollFooterShow() {
       $('footer.mobile').addClass('is-hidden')
     }
   });
+}
+
+function dateFormartPTBR(date) {
+    if(!date) return '';
+
+    separeted_date = date.split("/")
+    day = parseInt(separeted_date[0]);
+    month = parseInt(separeted_date[1]) - 1;
+    year = parseInt(separeted_date[2]);
+
+    date = new Date();
+    date.setDate(day);
+    date.setMonth(month);
+    date.setYear(year);
+    return date;
 }
