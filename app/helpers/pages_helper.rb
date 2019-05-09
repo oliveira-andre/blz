139cc34e7b137b9fc3cd6cfe42039709d9924b8b
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-05-09 16:41:34 -0400

    WIP

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,4 +5,11 @@ module PagesHelper
     background: -ms-linear-gradient(rgba(231, 231, 231, 0.2), rgba(0, 0, 0, 0.12), rgba(28, 130, 136, 0.87)), url('#{photo_url}');
     background: linear-gradient(rgba(231, 231, 231, 0.2), rgba(0, 0, 0, 0.12), rgba(28, 130, 136, 0.87)), url('#{photo_url}');"
   end
+
+  def liner_gradiente_card_img_dashboard_user(photo_url)
+    "background: -moz-linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.7)), url('#{photo_url}');
+    background: -webkit-linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.7)), url('#{photo_url}');
+    background: -ms-linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.7)), url('#{photo_url}');
+    background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.7)), url('#{photo_url}');"
+  end
 end
