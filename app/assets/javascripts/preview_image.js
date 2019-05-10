49c06017991bca4a5c70094ba6ab26a48c06f5e7
Gabriel Flauzino <flaugabriel@gmail.com>
2019-05-10 01:12:40 -0400

    remuve image by especific on input

--- app/assets/javascripts/preview_image.js
+++ app/assets/javascripts/preview_image.js
@@ -1,32 +1,41 @@
 $(document).on("turbolinks:load", function () {
-    $(document).ready(function() {
-        $(function(){
-            $('.delete-link').on('ajax:success', function() {
-                $(this).parents('div:first').remove();
-            });
-        });
-        if (window.File && window.FileList && window.FileReader) {
-            $("#files").on("change", function(e) {
-                var files = e.target.files,
-                    filesLength = files.length;
-                for (var i = 0; i < filesLength; i++) {
-                    var f = files[i]
-                    var fileReader = new FileReader();
-                    fileReader.onload = (function(e) {
-                        var file = e.target;
-                        console.log(e)
-                        $("<span class=\"pip\">" +
-                            "<img  class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
-                            "<br/><span class=\"remove\">Remover foto</span>" +
-                            "</span>").insertAfter("#files");
-                        $(".remove").click(function(){
-                            $(this).parent(".pip").remove();
-                        });
-                    });
-                    fileReader.readAsDataURL(f);
-                }
-            });
-        }
-    });
+  $('#files').on('change', function () {
 
+    var cont = 0
+    var files = this.files;
+
+    if (!files) return;
+    for (var i = 0; i < files.length; i++) {
+      var reader = new FileReader();
+      reader.onload = function (event) {
+        cont = i++
+        $(`<div class="column is-3" id="photo-${cont}">
+        <div class="card">
+          <div class="card-content is-inline">
+            <img class="image is-320x240" src="${event.target.result}">
+          </div>
+            <footer class="card-footer">
+            <a class="card-footer-item" onclick="remove(${cont})">Remove</a>
+            </footer>
+        </div>
+      </div>`).insertAfter($("#preview"));
+      }
+      reader.readAsDataURL(files.item(i));
+    }
+  });
+
+  $('#user-photo-add').on('change', function () {
+    var files = this.files;
+    if (!files) return;
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      var template_string = `<img class="is-rounded" src="${event.target.result}">`;
+      $("#temporary_image").html(template_string);
+    }
+    reader.readAsDataURL(files.item(0));
+  });
+  
 });
+function remove(img){
+  $("#photo-"+img).remove();
+}
