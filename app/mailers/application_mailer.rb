68bfb970c4223308ba2f229897e95f4c63fa1838
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-05-21 11:28:24 -0400

    fix application mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch('MAILER_SENDER')
+  default from: ENV.fetch('MAILER_SENDER', 'sistema@blz.rivelinojunior.com')
   layout 'mailer'
 end
