010427182e489a7f15258e75ddd1740ea565bd39
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-05-22 11:04:21 -0400

    add policy show to scheduling

--- app/policies/scheduling_policy.rb
+++ app/policies/scheduling_policy.rb
@@ -2,4 +2,9 @@ class SchedulingPolicy < ApplicationPolicy
   def create?
     user.establishment.nil?
   end
+
+  def show?
+    record.user == user ||
+      record.professional_service.service.establishment.user == user
+  end
 end
