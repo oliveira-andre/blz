47a3cd7f2d5c8c9eb2f738ed3c5a8afd492c8a4b
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-05-23 17:57:58 -0400

    create model payment card with your validations

---
+++ app/models/payment_card.rb
@@ -0,0 +1,15 @@
+class PaymentCard < ApplicationRecord
+  enum status: %i[active inactive]
+
+  belongs_to :user
+
+  validates :brand, presence: true
+  validates :moip_card_id, presence: true, uniqueness: { scope: :user_id }
+  validates :expiration_month, presence: true
+  validates :expiration_year, presence: true
+  validates :number, presence: true, uniqueness: { scope: :user_id }
+  validates :holder_name, presence: true
+  validates :holder_cpf, presence: true
+  validates :holder_birth_date, presence: true
+  validates_cpf :holder_cpf
+end
