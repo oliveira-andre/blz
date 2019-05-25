cbb4a3b5d130de0a1d2b55b9cdb0cde239eb146a
oliveira-andre <desmonplays@gmail.com>
2019-05-24 23:05:03 -0400

    (new feature) validating if is finished and if the current user is stored into user_id

--- app/policies/scheduling_policy.rb
+++ app/policies/scheduling_policy.rb
@@ -7,4 +7,8 @@ class SchedulingPolicy < ApplicationPolicy
     record.user == user ||
       record.professional_service.service.establishment.user == user
   end
+
+  def rating?
+    record.finished? && record.user == user
+  end
 end
