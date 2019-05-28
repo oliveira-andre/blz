c121713a615c4c260f15526b7ff6f3f77086ae7a
oliveira-andre <desmonplays@gmail.com>
2019-05-27 23:58:34 -0400

    (fix bug) switching from decimal to integer and adding the null false

--- db/migrate/20190522225555_reviews.rb
+++ db/migrate/20190522225555_reviews.rb
@@ -1,9 +1,9 @@
 class Reviews < ActiveRecord::Migration[5.2]
   def self.up
     create_table :reviews do |t|
-      t.text :body
+      t.text :body, null: false
       t.references :reviewable, polymorphic: true
-      t.decimal :rating
+      t.integer :rating, null: false
       t.references :user, foreign_key: true
 
       t.timestamp
