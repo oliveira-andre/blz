abdc9f5b7b0822655635f32af1f8511cf01ab82d
oliveira-andre <desmonplays@gmail.com>
2019-05-27 23:57:38 -0400

    (fix bug) removing the 255 maximum limit character

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :reviewable, polymorphic: true
   belongs_to :user
 
-  validates :body, presence: true, length: { maximum: 255 }
+  validates :body, presence: true
   validates :rating, presence: true,
             numericality: { greater_than_or_equal_to: 0,
                             less_than_or_equal_to: 5 }
