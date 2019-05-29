586057d2ef26bb52db2cd29d54ab1048c46d9ab4
oliveira-andre <desmonplays@gmail.com>
2019-05-29 02:02:32 -0400

    (new feature) applying the 0 as default in case of not select the star

--- db/migrate/20190522225555_reviews.rb
+++ db/migrate/20190522225555_reviews.rb
@@ -3,7 +3,7 @@ class Reviews < ActiveRecord::Migration[5.2]
     create_table :reviews do |t|
       t.text :body, null: false
       t.references :reviewable, polymorphic: true
-      t.integer :rating, null: false
+      t.integer :rating, null: false, default: 0
       t.references :user, foreign_key: true
 
       t.timestamp
