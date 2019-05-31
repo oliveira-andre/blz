7a22a6d7513477242e91aa42c2f8a11ed83ecf44
Gabriel Flauzino <flaugabriel@gmail.com>
2019-05-31 18:03:01 -0400

    add new atribute self_employed in db and view form

--- db/migrate/20190302192056_create_establishments.rb
+++ db/migrate/20190302192056_create_establishments.rb
@@ -5,7 +5,7 @@ class CreateEstablishments < ActiveRecord::Migration[5.2]
       t.string :timetable, null: false
       t.integer :status, default: 0
       t.references :user, foreign_key: true
-
+      t.integer :self_employed, null: false
       t.timestamps
     end
   end
