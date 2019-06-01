dd3b9a420561c9ee5a9ec9ea3b6b990fa0010bf6
Gabriel Flauzino <flaugabriel@gmail.com>
2019-05-31 23:32:47 -0400

    set validates in model

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -3,4 +3,16 @@ class Professional < ApplicationRecord
   has_many :professional_services
   has_many :office_hours
   has_one_attached :photo
+
+  validate :restrict_establishment
+
+  private
+
+  def restrict_establishment
+    cont_arry = []
+    cont_arry.push(establishment.user_id)
+    if establishment.self_employed == 0
+      errors.add(:base, 'Apenas um profissional!') if cont_arry.length == 1
+    end
+  end
 end
