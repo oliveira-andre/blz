5c6100ba7fd4245ef845de8bd8cd42f6d092af3e
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-06-07 10:18:03 -0400

    create job to notification broadcast

---
+++ app/jobs/notification_broadcast_job.rb
@@ -0,0 +1,13 @@
+class NotificationBroadcastJob < ApplicationJob
+  queue_as :default
+
+  def perform(scheduling)
+    notification_name = "notification:#{scheduling.service.establishment
+                                                  .user.email}"
+    date = "#{I18n.l(scheduling.date, format: :day_month)} às" \
+           "#{I18n.l(scheduling.date, format: :time)}"
+
+    ActionCable.server.broadcast notification_name,
+                                 message: "Novo agendamento para #{date}"
+  end
+end
