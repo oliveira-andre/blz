88834e749e483077988426c2209da21f17f9ee40
oliveira-andre <desmonplays@gmail.com>
2019-06-10 23:50:04 -0400

    (new feature) adding show and destroy with a the before action that find the payment_card by params id

--- app/controllers/payment_cards_controller.rb
+++ app/controllers/payment_cards_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PaymentCardsController < ApplicationController
+  before_action :load_payment_card, only: %i[show destroy]
   def index
     @payment_cards = current_user.payment_cards
   end
@@ -15,11 +16,21 @@ class PaymentCardsController < ApplicationController
     redirect_to payment_cards_path
   end
 
+  def show; end
+
+  def destroy
+    redirect_to payment_cards_path, notice: 'Excluído' if @payment_card.destroy
+  end
+
   private
 
   def card_params
     params.require(:payment_card).permit(:number, :holder_name, :holder_cpf,
                                          :expiration_month, :expiration_year,
                                          :hash_card, :holder_birth_date)
+  end
+
+  def load_payment_card
+    @payment_card = PaymentCard.find(params[:id])
   end
 end
