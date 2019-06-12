315305ec61d36d77a6e3cdeff035b9e99db37dd3
oliveira-andre <desmonplays@gmail.com>
2019-06-12 15:39:57 -0400

    (fix bug) adding select range into year and month expiration

--- app/helpers/payment_card_helper.rb
+++ app/helpers/payment_card_helper.rb
@@ -4,4 +4,12 @@ module PaymentCardHelper
   def payment_card_number(number)
     '**** **** **** ' + number.last(4)
   end
+
+  def credit_card_expiration_year_range
+    (Time.now.strftime("%y").to_i..(Time.now.strftime("%y").to_i + 15)).to_a
+  end
+
+  def credit_card_expiration_month_range
+    (1..12).to_a
+  end
 end
