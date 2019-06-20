66d80581037f683cb349591f462caa6f28270068
oliveira-andre <desmonplays@gmail.com>
2019-06-20 16:50:40 -0400

    (new feature) set show and load the @scheduling

--- app/controllers/admin/scheduling_controller.rb
+++ app/controllers/admin/scheduling_controller.rb
@@ -1,8 +1,19 @@
+# frozen_string_literal: true
+
 module Admin
   class SchedulingController < AdminController
+    before_action :scheduling, only: :show
     def index
       @shedulings = Scheduling.all
-      @pagy, @records  = pagy_countless(@shedulings)
+      @pagy, @records = pagy_countless(@shedulings)
+    end
+
+    def show; end
+
+    private
+
+    def scheduling
+      @scheduling = Scheduling.find(params[:id])
     end
   end
 end
