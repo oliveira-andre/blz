70d05b27d1c6d51ded7a706ea88cbd1b16a43053
oliveira-andre <desmonplays@gmail.com>
2019-06-20 17:37:05 -0400

    (new feature) updating and showing the service

--- app/controllers/admin/services_controller.rb
+++ app/controllers/admin/services_controller.rb
@@ -1,5 +1,7 @@
 module Admin
   class ServicesController < AdminController
+    before_action :service, only: %i[show update]
+
     def index
       if params[:status].present?
         @services = Service.where(status: params[:status]) || Service.all
@@ -7,6 +9,24 @@ module Admin
         @services = Service.search(params[:query]) || Service.all
       end
       @pagy, @services = pagy(@services)
+    end
+
+    def show; end
+
+    def update
+      @service.update(status: params[:status])
+      redirect_to admin_services_path
+      flash[:success] = if params[:status] == 'approved'
+                          'Aprovado com sucesso'
+                        else
+                          'Recusado com sucesso'
+                        end
+    end
+
+    private
+
+    def service
+      @service = Service.find(params[:id])
     end
   end
 end
