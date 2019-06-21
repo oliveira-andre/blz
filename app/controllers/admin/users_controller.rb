69055246ad0945e9a69803aafc905afa6d86f023
oliveira-andre <desmonplays@gmail.com>
2019-06-21 17:50:13 -0400

    (new feature) showing the updating the user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,12 +1,34 @@
+# frozen_string_literal: true
+
 module Admin
   class UsersController < AdminController
+    before_action :user, only: %i[show update]
+
     def index
-      if params[:search]
-        @users = User.search(params[:search])
-      else
-        @users = User.all
-      end
+      @users = if params[:search]
+                 User.search(params[:search])
+               else
+                 User.all
+               end
       @pagy, @users = pagy(@users)
+    end
+
+    def show; end
+
+    def update
+      @user.update(status: params[:status])
+      redirect_to admin_users_path
+      flash[:success] = if params[:status] == 'active'
+                          'Ativado com sucesso'
+                        else
+                          'Bloqueado com sucesso'
+                        end
+    end
+
+    private
+
+    def user
+      @user = User.find(params[:id])
     end
   end
 end
