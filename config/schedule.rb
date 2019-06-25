256aca3b2d8631313fe990feb1cfdbdc8e55ee0e
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-06-25 18:21:07 -0400

    fix schedule to backup

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ every 1.day, at: '00:01 am' do
   runner 'RebuildAllSchedulesService.execute'
 end
 
-every 1.hour do
+every :hour do
   command 'backup perform -c /var/www/blz/backup/config.rb --trigger blzbackup'
 end
