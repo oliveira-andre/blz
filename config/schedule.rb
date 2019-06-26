744c1f662b330ebe30c4f3c8853e9a27f3699d26
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-06-26 18:14:00 -0400

    fix time to schedule crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ every 1.day, at: '00:01 am' do
   runner 'RebuildAllSchedulesService.execute'
 end
 
-every :hour do
+every 59.minutes do
   command 'backup perform -c /var/www/blz/backup/config.rb --trigger blzbackup'
 end
