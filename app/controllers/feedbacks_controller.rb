fae4d842ed551e15e97d9b40bfef85d472c543c1
oliveira-andre <desmonplays@gmail.com>
2019-06-27 17:23:37 -0400

    (new feature) load_establishment

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,6 +2,13 @@
 
 class FeedbacksController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :load_establishment, only: :index
 
   def index; end
+
+  private
+
+  def load_establishment
+    @establishment = Establishment.find(params[:establishment_id])
+  end
 end
