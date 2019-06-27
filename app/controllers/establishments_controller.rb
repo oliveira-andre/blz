9e8f38519b01554e0e0b498999b20cd8a17bd512
oliveira-andre <desmonplays@gmail.com>
2019-06-27 17:23:27 -0400

    (new feature) removing notice

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -12,8 +12,7 @@ class EstablishmentsController < ApplicationController
     @establishment = Establishment.new establishment_params
     @establishment.save!
 
-    redirect_to feedbacks_path,
-                notice: 'Cadastro realizado com sucesso'
+    redirect_to establishment_feedbacks_path(@establishment)
   rescue ActiveRecord::RecordInvalid => error
     @messages_errors = error.record.errors.full_messages
     render 'new'
