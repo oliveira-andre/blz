9cc1302f687bcb45e567c7075e3998905a53f3f9
oliveira-andre <desmonplays@gmail.com>
2019-06-28 16:11:57 -0400

    (new feature) routing admin_establishments

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,6 @@ Rails.application.routes.draw do
     resources :scheduling, only: %i[index show]
     resources :services, only: %i[index show update]
     resources :users, only: %i[index show update]
+    resources :establishments, only: :index
   end
 end
