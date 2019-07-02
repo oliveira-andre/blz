6ea6605b9ac18c38d250cc4bc83e0ab22eced0bb
oliveira-andre <desmonplays@gmail.com>
2019-07-02 16:06:33 -0400

    (new feature) adding ids

--- app/models/scheduling.rb
+++ app/models/scheduling.rb
@@ -49,7 +49,7 @@ class Scheduling < ApplicationRecord
     scheduling_ids = Scheduling.where(
       professional_service_id: professional_services_ids,
       date: (date..(date + service_duration.minutes - 1.seconds))
-    ).scheduled
+    ).scheduled.ids
 
     return if scheduling_ids.empty?
 
