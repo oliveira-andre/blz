6cc261a3e4ba0fdee7e1449b252f9a3a4520ec02
oliveira-andre <desmonplays@gmail.com>
2019-07-08 13:29:51 -0400

    (new feature) utc date params

--- app/controllers/busies_controller.rb
+++ app/controllers/busies_controller.rb
@@ -1,20 +1,32 @@
+# frozen_string_literal: true
+
 class BusiesController < ApplicationController
   def create
-    @scheduling = Scheduling.new(scheduling_params).busy!
-
-    if @scheduling.save
-      flash[:success] = 'Profissional ocupado com sucesso'
-    else
-      @scheduling.errors.full_messages.each { |error| flash[:error] = error }
+    professional_services.each do |professional_service|
+      @scheduling = Scheduling.new(scheduling_params.merge(
+                                     professional_service: professional_service
+                                   ))
+      @scheduling.busy!
     end
-
+    flash[:success] = 'Profissional ocupado com sucesso'
+    redirect_to establishments_dashboard_path(@scheduling.service.establishment)
+  rescue ActiveRecord::RecordInvalid => e
+    @scheduling.errors.full_messages.each { |error| flash[:error] = error }
     redirect_to establishments_dashboard_path(@scheduling.service.establishment)
   end
 
   private
 
   def scheduling_params
-    params.permit(:establishment_professionals, :service_duration)
-          .merge(date: "#{params[:date_only]} #{params[:time]}".to_time)
+    params.permit(:service_duration)
+          .merge(
+            date: "#{params[:date_only]} #{params[:time]} UTC".to_time
+          )
+  end
+
+  def professional_services
+    @professional_services ||= ProfessionalService.where(
+      professional: params[:professional]
+    )
   end
 end
