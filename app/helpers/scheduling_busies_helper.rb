942e5bd127faea2439d399d2f7f568475ba0eab9
oliveira-andre <desmonplays@gmail.com>
2019-07-08 21:19:52 -0400

    (new feature) removing one month

--- app/helpers/scheduling_busies_helper.rb
+++ app/helpers/scheduling_busies_helper.rb
@@ -7,7 +7,6 @@ module SchedulingBusiesHelper
     duration_busy_range << ['3 dia', 4320]
     duration_busy_range << ['1 semana', 10_080]
     duration_busy_range << ['2 semanas', 20_160]
-    duration_busy_range << ['1 mês', 43_200]
     duration_busy_range
   end
 end
