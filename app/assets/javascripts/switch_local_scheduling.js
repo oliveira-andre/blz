a50ba5ddcedfc970239facb7b5a9d216db14bfa3
oliveira-andre <desmonplays@gmail.com>
2019-07-08 21:18:09 -0400

    (new feature) switching from home and stablishment

---
+++ app/assets/javascripts/switch_local_scheduling.js
@@ -0,0 +1,16 @@
+$(document).on('turbolinks:load', function () {
+  switchLocalScheduling();
+  $('#scheduling_home').change(function () {
+    switchLocalScheduling();
+  });
+});
+
+function switchLocalScheduling(select) {
+  if ($('#scheduling_home').val() == 1) {
+    $('#establishment_address').addClass('is-hidden')
+    $('#address_form').removeClass('is-hidden');
+  } else if($('#scheduling_home').val() == 0) {
+    $('#establishment_address').removeClass('is-hidden')
+    $('#address_form').addClass('is-hidden');
+  }
+}
