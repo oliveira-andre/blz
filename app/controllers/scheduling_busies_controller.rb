5b97148578bc0c74cf01b36b4408eb37aac58490
oliveira-andre <desmonplays@gmail.com>
2019-07-09 18:34:23 -0400

    (new feature) fix terminal

--- app/controllers/scheduling_busies_controller.rb
+++ app/controllers/scheduling_busies_controller.rb
@@ -2,11 +2,17 @@
 
 class SchedulingBusiesController < ApplicationController
   def create
-    SchedulingBusiesService.execute(params)
+    SchedulingBusiesService.execute(scheduling_params)
     flash[:success] = 'Período marcado como indisponível'
     redirect_to establishments_dashboard_path(params[:establishment_id])
   rescue ActiveRecord::RecordInvalid => e
     e.record.errors.full_messages.each { |error| flash[:error] = error }
     redirect_to establishments_dashboard_path(params[:establishment_id])
+  end
+
+  private
+
+  def scheduling_params
+    params.permit(:service_duration, :date_only, :time, :professional_id)
   end
 end
