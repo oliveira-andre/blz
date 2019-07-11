10e4adfb43453177ddd398cbe329fe8d55cb4c5d
oliveira-andre <desmonplays@gmail.com>
2019-07-10 22:05:03 -0400

    (new feature) removing additional code

--- app/assets/javascripts/switch_local_scheduling.js
+++ app/assets/javascripts/switch_local_scheduling.js
@@ -9,16 +9,10 @@ function switchLocalScheduling(select) {
   if ($('#scheduling_home').val() == 1) {
     $('#establishment_address').addClass('is-hidden');
     $('#address_form').removeClass('is-hidden');
-    $('#scheduling_address_attributes_zipcode').prop('required', true);
-    $('#scheduling_address_attributes_street').prop('required', true);
-    $('#scheduling_address_attributes_neighborhood').prop('required', true);
-    $('#scheduling_address_attributes_number').prop('required', true);
+    $('#address_field').prop('required', true);
   } else if($('#scheduling_home').val() == 0) {
     $('#establishment_address').removeClass('is-hidden');
     $('#address_form').addClass('is-hidden');
-    $('#scheduling_address_attributes_zipcode').prop('required', false);
-    $('#scheduling_address_attributes_street').prop('required', false);
-    $('#scheduling_address_attributes_neighborhood').prop('required', false);
-    $('#scheduling_address_attributes_number').prop('required', false);
+    $('#address_field').prop('required', false);
   }
 }
