c7c1aafb1b3aeae0efb5fbd6a02fd01bb90cea2a
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-07-14 16:42:13 -0300

    config production environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,42 +1,7 @@
-# config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
 set :application, "blz"
 set :repo_url, "git@gitlab.com:blz/blz.git"
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
 
 set :deploy_to, "/var/www/blz"
 append :linked_dirs, "log", "tmp"
@@ -49,7 +14,6 @@ set :puma_access_log, "#{shared_path}/log/puma_access.log"
 set :puma_error_log, "#{shared_path}/log/puma_error.log"
 set :nginx_sites_available_path, "/etc/nginx/sites-available"
 set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
-set :rvm_ruby_version, '2.6.1'
 
 namespace :puma do
   desc 'Create Puma dirs'
