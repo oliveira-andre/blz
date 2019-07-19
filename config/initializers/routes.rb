be0837f2273afbc61ba4340cf09ad69806b9584d
oliveira-andre <desmonplays@gmail.com>
2019-07-19 16:18:19 -0400

    (new feature) adding the localized to push the locale

---
+++ config/initializers/routes.rb
@@ -0,0 +1,7 @@
+RouteTranslator.config do |config|
+  config.available_locales = ['pt-BR']
+  config.force_locale = true
+  config.hide_locale = true
+  config.locale_param_key = 'pt-BR'
+  config.generate_unnamed_unlocalized_routes = true
+end
