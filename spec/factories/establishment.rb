73560ae841eb13070d15c1b77d40a60e9b26c36b
oliveira-andre <desmonplays@gmail.com>
2019-07-24 11:29:50 -0400

    (new feature) trait to establishment

--- spec/factories/establishment.rb
+++ spec/factories/establishment.rb
@@ -1,8 +1,21 @@
 FactoryBot.define do
   factory :establishment do
-    name          { FFaker::Lorem.word }
-    timetable     { 'Das 00:00 às 12:00' }
-    status        { :approved }
-    association   :user, :establishment
+
+    trait :approved do
+      name          { FFaker::Lorem.word }
+      timetable     { 'Das 00:00 às 12:00' }
+      status        { :approved }
+      association   :user, :establishment
+    end
+
+    trait :disapproved do
+      name          { FFaker::Lorem.word }
+      timetable     { 'Das 00:00 às 12:00' }
+      status        { :disapproved }
+      association   :user, :establishment
+    end
+
+    factory :approved_establishment, traits: [:approved]
+    factory :disapproved_establishment, traits: [:disapproved]
   end
 end
