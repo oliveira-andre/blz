672269fc79f784ab766d3dfae7a8528041713367
Rivelino junior <rv.fernandes.junior@gmail.com>
2019-07-24 12:21:49 -0400

    add column about to establishment

---
+++ db/migrate/20190723144244_add_column_about_to_establishment.rb
@@ -0,0 +1,5 @@
+class AddColumnAboutToEstablishment < ActiveRecord::Migration[5.2]
+  def change
+    add_column :establishments, :about, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_172713) do
+ActiveRecord::Schema.define(version: 2019_07_23_144244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_07_18_172713) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "self_employed", default: false
+    t.text "about"
     t.index ["user_id"], name: "index_establishments_on_user_id"
   end
 
