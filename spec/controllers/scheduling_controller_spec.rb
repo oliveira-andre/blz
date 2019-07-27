0cf1f23a1407deb0058189aaed6698f85813bff8
oliveira-andre <desmonplays@gmail.com>
2019-07-26 20:43:26 -0400

    (new feature) complementar tests

--- spec/controllers/scheduling_controller_spec.rb
+++ spec/controllers/scheduling_controller_spec.rb
@@ -65,6 +65,16 @@ RSpec.describe SchedulingController, type: :controller do
   end
 
   describe 'create scheduling' do
+    context "when user isn't authenticated" do
+      it 'redirect to sign in' do
+        get :create
+        expect(flash[:alert]).to eq(
+          'Para continuar, efetue login ou registre-se.'
+        )
+        expect(response).to redirect_to(new_user_session_pt_br_path)
+      end
+    end
+
     context 'when user is authenticated' do
       let(:schedule) { FactoryBot.create(:schedule) }
 
@@ -113,19 +123,168 @@ RSpec.describe SchedulingController, type: :controller do
         end
       end
 
-      context 'when user registration is ok' do
-        it 'create the scheduling with success' do
-          user = FactoryBot.create(:completed_user)
-          sign_in user
-          params = { scheduling:
-                    { professional_service_id: schedule.professional_service.id,
-                      date: schedule.date, in_home: 0 } }
-          post :create, params: params
+      context "when date isn't on schedule" do
+        it 'show error message staying in the same page' do
+          sign_in FactoryBot.create(:completed_user)
+
+          post :create, params: { scheduling: {
+            professional_service_id: schedule.professional_service.id,
+            date: (schedule.date + 1.month), in_home: 0
+          } }
           expect(response).to redirect_to(
-            users_dashboard_pt_br_path(id: user.id)
+            new_scheduling_pt_br_path(
+              date: (schedule.date + 1.month),
+              professional_service_id: schedule.professional_service.id
+            )
           )
-          expect(flash[:error]).to eq(nil)
-          expect(flash[:success]).to eq('Agendamento realizado com sucesso!')
+          expect(flash[:error]).to eq(
+            'Horário inválido ou indisponível, tente novamente'
+          )
+        end
+      end
+
+      context "when date isn't on schedule" do
+        it 'show error message staying in the same page' do
+          sign_in FactoryBot.create(:completed_user)
+
+          post :create, params: { scheduling: {
+            professional_service_id: schedule.professional_service.id,
+            date: (schedule.date + 1.month), in_home: 0
+          } }
+          expect(response).to redirect_to(
+            new_scheduling_pt_br_path(
+              date: (schedule.date + 1.month),
+              professional_service_id: schedule.professional_service.id
+            )
+          )
+          expect(flash[:error]).to eq(
+            'Horário inválido ou indisponível, tente novamente'
+          )
+        end
+      end
+
+      context "when date isn't on schedule" do
+        it 'show error message staying in the same page' do
+          sign_in FactoryBot.create(:completed_user)
+
+          post :create, params: { scheduling: {
+            professional_service_id: schedule.professional_service.id,
+            date: (schedule.date + 1.month), in_home: 0
+          } }
+          expect(response).to redirect_to(
+            new_scheduling_pt_br_path(
+              date: (schedule.date + 1.month),
+              professional_service_id: schedule.professional_service.id
+            )
+          )
+          expect(flash[:error]).to eq(
+            'Horário inválido ou indisponível, tente novamente'
+          )
+        end
+      end
+
+      context 'when professional already busy' do
+        it 'show error message staying in the same page' do
+          @scheduling = FactoryBot.create(:scheduling)
+          sign_in FactoryBot.create(:completed_user)
+
+          post :create, params: { scheduling: {
+            professional_service_id: @scheduling.professional_service.id,
+            date: @scheduling.date, in_home: 0
+          } }
+          expect(response).to redirect_to(
+            new_scheduling_pt_br_path(
+              date: @scheduling.date,
+              professional_service_id: @scheduling.professional_service.id
+            )
+          )
+          expect(flash[:error]).to eq(
+            'Horário já esta ocupado para esse salão/profissional'
+          )
+        end
+      end
+
+      context "when isn't in home" do
+        context 'when user registration is ok' do
+          it 'create the scheduling with success' do
+            user = FactoryBot.create(:completed_user)
+            sign_in user
+            params = { scheduling:
+                      { professional_service_id: schedule.professional_service.id,
+                        date: schedule.date, in_home: 0 } }
+            post :create, params: params
+            expect(response).to redirect_to(
+              users_dashboard_pt_br_path(id: user.id)
+            )
+            expect(flash[:error]).to eq(nil)
+            expect(flash[:success]).to eq('Agendamento realizado com sucesso!')
+          end
+        end
+      end
+
+      context 'when is in home' do
+        context 'when the service not accept in home' do
+          it 'show error and stay in the same page' do
+            sign_in FactoryBot.create(:completed_user)
+
+            post :create, params: { scheduling: {
+              professional_service_id: schedule.professional_service.id,
+              date: schedule.date, in_home: 1
+            } }
+            expect(response).to redirect_to(
+              new_scheduling_pt_br_path(
+                date: schedule.date,
+                professional_service_id: schedule.professional_service.id
+              )
+            )
+            expect(flash[:error]).to eq(
+              'Serviço não permite essa localidade'
+            )
+          end
+        end
+
+        context 'without address' do
+          it 'show error and stay in the same page' do
+            sign_in FactoryBot.create(:completed_user)
+            home_schedule = FactoryBot.create(:schedule_in_home)
+
+            post :create, params: { scheduling: {
+              professional_service_id: home_schedule.professional_service.id,
+              date: home_schedule.date, in_home: 1,
+              address_attributes: { street: '' }
+            } }
+            expect(response).to redirect_to(
+              new_scheduling_pt_br_path(
+                date: home_schedule.date,
+                professional_service_id: home_schedule.professional_service.id
+              )
+            )
+            expect(flash[:error]).to eq(
+              'Bairro não pode ficar em branco'
+            )
+          end
+        end
+
+        context 'with address' do
+          it 'create the scheduling with success' do
+            user = FactoryBot.create(:completed_user)
+            sign_in user
+            home_schedule = FactoryBot.create(:schedule_in_home)
+
+            post :create, params: { scheduling: {
+              professional_service_id: home_schedule.professional_service.id,
+              date: home_schedule.date, in_home: 1,
+              address_attributes: { 
+                zipcode: '76807152', street: 'teste', neighborhood: 'teste',
+                number: '4135'
+              }
+            } }
+            expect(response).to redirect_to(
+              users_dashboard_pt_br_path(id: user.id)
+            )
+            expect(flash[:error]).to eq(nil)
+            expect(flash[:success]).to eq('Agendamento realizado com sucesso!')
+          end
         end
       end
     end
