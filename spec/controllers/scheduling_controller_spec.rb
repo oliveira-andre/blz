e6c5c0bcdd1bffa334c1994f9e153c7719bb8c5b
oliveira-andre <desmonplays@gmail.com>
2019-07-29 11:50:50 -0400

    (new feature) problems with user test

--- spec/controllers/scheduling_controller_spec.rb
+++ spec/controllers/scheduling_controller_spec.rb
@@ -301,4 +301,29 @@ RSpec.describe SchedulingController, type: :controller do
       end
     end
   end
+
+  describe 'destroy scheduling' do
+    context "when user isn't authenticated" do
+      it 'redirect to sign in' do
+        delete :destroy, params: { id: scheduling.id }
+        expect(flash[:alert]).to eq(
+          'Para continuar, efetue login ou registre-se.'
+        )
+        expect(response).to redirect_to(new_user_session_pt_br_path)
+      end
+    end
+
+    context 'when user is authenticated' do
+      let(:scheduling) { FactoryBot.create(:scheduling) }
+
+      context 'when user is blocked' do
+        it 'redirect to root page, not login and show error' do
+          sign_in FactoryBot.create(:blocked_user)
+          delete :destroy, params: { id: scheduling.id }
+          expect(flash[:error]).to eq('Seu usuário está bloqueado')
+          expect(response).to redirect_to(root_path)
+        end
+      end
+    end
+  end
 end
