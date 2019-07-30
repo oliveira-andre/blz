34a8f54fa6f29f651d503b64d6c7a1afb9d6949c
oliveira-andre <desmonplays@gmail.com>
2019-07-30 16:44:04 -0400

    (new feature) finishe scheduling update

--- spec/controllers/scheduling_controller_spec.rb
+++ spec/controllers/scheduling_controller_spec.rb
@@ -385,4 +385,69 @@ RSpec.describe SchedulingController, type: :controller do
       end
     end
   end
+
+  describe 'update scheduling' do
+    let(:scheduling) { FactoryBot.create(:scheduling) }
+
+    context "when user isn't authenticated" do
+      it 'redirect to sign in' do
+        post :update, params: { id: scheduling.id }
+        expect(flash[:alert]).to eq(
+          'Para continuar, efetue login ou registre-se.'
+        )
+        expect(response).to redirect_to(new_user_session_pt_br_path)
+      end
+    end
+
+    context 'when user is authenticated' do
+      context 'when user is blocked' do
+        it 'redirect to root page, not login and show error' do
+          sign_in FactoryBot.create(:blocked_user)
+          post :update, params: { id: scheduling.id }
+          expect(flash[:error]).to eq('Seu usuário está bloqueado')
+          expect(response).to redirect_to(root_path)
+        end
+      end
+
+      context "when user don't have relation with scheduling" do
+        it 'show error and redirect to root_path' do
+          sign_in FactoryBot.create(:user)
+          delete :update, params: { id: scheduling.id }
+          expect(flash[:error]).to eq('Não autorizado')
+          expect(response).to redirect_to(root_path)
+        end
+      end
+
+      context 'when user is the stablishment of scheduling' do
+        context 'establishment user try to finish before the scheduling date' do
+          it 'show error staying in the same page' do
+            sign_in scheduling.professional_service.service.establishment.user
+            post :update, params: { id: scheduling.id, status: :finished }
+            expect(flash[:error]).to eq(
+              'Agendamento não pode ser finalizado antes a data combinada'
+            )
+            expect(response).to redirect_to(
+              establishments_dashboard_pt_br_path(
+                scheduling.professional_service.service.establishment
+              )
+            )
+          end
+        end
+
+        context 'when the date is after the scheduling date' do
+          it 'finalize with success and show successs' do
+            travel_to scheduling.date + 1.hour
+            sign_in scheduling.professional_service.service.establishment.user
+            post :update, params: { id: scheduling.id, status: :finished }
+            expect(flash[:success]).to eq('Finalizado com sucesso')
+            expect(response).to redirect_to(
+              establishments_dashboard_pt_br_path(
+                scheduling.professional_service.service.establishment
+              )
+            )
+          end
+        end
+      end
+    end
+  end
 end
