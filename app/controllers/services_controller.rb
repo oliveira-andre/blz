b7db84cf309c0c9cdf8ceb51392c0a75a9da1535
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-08-02 09:18:10 -0400

    add authorize to index service

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,10 +3,13 @@
 class ServicesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show]
 
-  before_action :load_establishment, only: %i[index new create]
+  before_action :load_establishment, only: %i[new create]
   before_action :load_service, only: %i[edit update destroy]
 
-  def index; end
+  def index
+    authorize current_user, policy_class: ServicePolicy
+    @services = current_user.establishment.services
+  end
 
   def show
     @service = Service.approved.find(params[:id])
