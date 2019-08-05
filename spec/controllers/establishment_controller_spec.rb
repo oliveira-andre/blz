7b8b0effecfb0d08249d8011baebfafc2a1a1520
oliveira-andre <desmonplays@gmail.com>
2019-08-05 18:22:14 -0400

    (new feature) switching params to factory bot

--- spec/controllers/establishment_controller_spec.rb
+++ spec/controllers/establishment_controller_spec.rb
@@ -11,31 +11,15 @@ RSpec.describe EstablishmentsController, type: :controller do
     context 'without accept the term' do
       it 'show error and not create the establishment' do
         post :create, params: {
-          establishment: {
-            name: FFaker::BaconIpsum.word,
-            self_employed: 0,
-            timetable: 'Das 08:00 às 18:00',
-            address_attributes: {
-              zipcode: FFaker::AddressBR.zip_code,
-              street: FFaker::AddressBR.street_name,
-              neighborhood: FFaker::AddressBR.neighborhood,
-              number: FFaker::AddressBR.building_number
-            },
-            user_attributes: {
-              name: FFaker::BaconIpsum.word,
-              cpf: FFaker::IdentificationBR.cpf,
-              email: FFaker::Internet.email,
-              birth_date: FFaker::Time.between(25.years.ago, 18.years.ago),
-              phone: FFaker::PhoneNumberCH.mobile_phone_number,
-              password: 'secret123',
-              password_confirmation: 'secret123',
-              terms_acceptation: 0
-            }
-          }
+          establishment: FactoryBot
+            .attributes_for(:establishment).merge(
+              user_attributes: FactoryBot.attributes_for(:without_term_user),
+              address_attributes: FactoryBot.attributes_for(:address)
+            )
         }
         subject.stub(:create)
-        expect(assigns(:messages_errors)).to eq(
-          ['Termos de uso não pode ficar em branco']
+        expect(assigns(:messages_errors)).to include(
+          'Termos de uso não pode ficar em branco'
         )
       end
     end
@@ -43,65 +27,31 @@ RSpec.describe EstablishmentsController, type: :controller do
     context 'without address fields' do
       it 'show error and not create the establishment' do
         post :create, params: {
-          establishment: {
-            name: FFaker::BaconIpsum.word,
-            self_employed: 0,
-            timetable: 'Das 08:00 às 18:00',
-            address_attributes: {
-              zipcode: '',
-              street: '',
-              neighborhood: '',
-              number: ''
-            },
-            user_attributes: {
-              name: FFaker::BaconIpsum.word,
-              cpf: FFaker::IdentificationBR.cpf,
-              email: FFaker::Internet.email,
-              birth_date: FFaker::Time.between(25.years.ago, 18.years.ago),
-              phone: FFaker::PhoneNumberCH.mobile_phone_number,
-              password: 'secret123',
-              password_confirmation: 'secret123',
-              terms_acceptation: 1
-            }
-          }
+          establishment: FactoryBot
+            .attributes_for(:establishment).merge(
+              user_attributes: FactoryBot.attributes_for(:completed_user),
+              address_attributes: FactoryBot.attributes_for(:empty_address)
+            )
         }
         subject.stub(:create)
-        expect(assigns(:messages_errors)).to eq(
-          ['Rua/Logradouro não pode ficar em branco',
-           'Número não pode ficar em branco',
-           'Bairro não pode ficar em branco']
+        expect(assigns(:messages_errors)).to include(
+          'Rua/Logradouro não pode ficar em branco'
         )
       end
     end
 
     context 'completing all fields correctly' do
       it 'create with success' do
-        random_email = FFaker::Internet.email
+        user = FactoryBot.attributes_for(:completed_user)
         post :create, params: {
-          establishment: {
-            name: FFaker::BaconIpsum.word,
-            self_employed: 0,
-            timetable: 'Das 08:00 às 18:00',
-            address_attributes: {
-              zipcode: FFaker::AddressBR.zip_code,
-              street: FFaker::AddressBR.street_name,
-              neighborhood: FFaker::AddressBR.neighborhood,
-              number: FFaker::AddressBR.building_number
-            },
-            user_attributes: {
-              name: FFaker::BaconIpsum.word,
-              cpf: FFaker::IdentificationBR.cpf,
-              email: random_email,
-              birth_date: FFaker::Time.between(25.years.ago, 18.years.ago),
-              phone: FFaker::PhoneNumberCH.mobile_phone_number,
-              password: 'secret123',
-              password_confirmation: 'secret123',
-              terms_acceptation: 1
-            }
-          }
+          establishment: FactoryBot
+            .attributes_for(:establishment).merge(
+              user_attributes: user,
+              address_attributes: FactoryBot.attributes_for(:address)
+            )
         }
         expect(response).to redirect_to(
-          feedbacks_pt_br_path(email: random_email)
+          feedbacks_pt_br_path(email: user[:email])
         )
       end
     end
