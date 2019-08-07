0ba69ddca6db74c1f3b08aa3ff48d654cfe98932
oliveira-andre <desmonplays@gmail.com>
2019-08-07 17:34:33 -0400

    (new feature) fix the error by scheduling in_home

--- spec/factories/scheduling.rb
+++ spec/factories/scheduling.rb
@@ -1,11 +1,15 @@
 FactoryBot.define do
   factory :scheduling do
-    in_home { 0 }
     association :user, :completed
     after(:build) do |scheduling|
       schedule = create(:schedule)
       scheduling.professional_service = schedule.professional_service
       scheduling.date = schedule.date
+      if schedule.professional_service.service.home?
+        scheduling.in_home = 1
+      else
+        scheduling.in_home = 0
+      end
     end
 
     trait :busy do
