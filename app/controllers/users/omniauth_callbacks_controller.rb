d562b60665ae6160769f7814a3328c7c352fc328
Rivelino junior <rv.fernandes.junior@gmail.com>
2019-08-14 12:34:06 -0400

    add flash messages errors

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,12 +9,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
       sign_in_and_redirect @user, event: :authentication
     else
+      @user&.errors&.full_messages&.each { |error| flash[:error] = error }
       session['devise.facebook_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+      redirect_to new_user_session_path
     end
   end
 
   def failure
+    @user&.errors&.full_messages&.each { |error| flash[:error] = error }
     redirect_to root_path
   end
 end
