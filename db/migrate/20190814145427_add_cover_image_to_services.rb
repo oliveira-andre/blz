4602c1184398e8189ebbdd08ec5d940185fd42d3
oliveira-andre <desmonplays@gmail.com>
2019-08-14 15:39:20 -0400

    (new feature) initializing the cover image

---
+++ db/migrate/20190814145427_add_cover_image_to_services.rb
@@ -0,0 +1,9 @@
+class AddCoverImageToServices < ActiveRecord::Migration[5.2]
+  def self.up
+    add_column :services, :cover_image_id, :integer
+  end
+
+  def self.down
+    remove_column :services, :cover_image_id
+  end
+end
