44a5735556c7bf0b9cf1d4f559fee7ba40974425
oliveira-andre <desmonplays@gmail.com>
2019-08-22 11:56:37 -0400

    (new feature) fix message

--- app/services/cancel_scheduling_timed_out.rb
+++ app/services/cancel_scheduling_timed_out.rb
@@ -7,15 +7,12 @@ module CancelSchedulingTimedOut
         if time > scheduling.date
           scheduling.update(
             status: :canceled, canceled_at: time, canceled_by: 1,
-            canceled_reason: canceled_sentece
+            canceled_reason: 'Cancelado pelo sistema,'\
+                              'devido ter passado da data combinada'
           )
           scheduling.professional_service.service.establishment.user.blocked!
         end
       end
-    end
-
-    def canceled_sentece
-      'Cancelado pelo sistema, devido ter passado da data combinada'
     end
   end
 end
