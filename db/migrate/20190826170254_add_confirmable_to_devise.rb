ecce935522f0f1b2496d36a71ae0ebcfb0a0371b
oliveira-andre <desmonplays@gmail.com>
2019-08-26 17:05:08 -0400

    (new feature) adding unconfirmed_mail

--- db/migrate/20190826170254_add_confirmable_to_devise.rb
+++ db/migrate/20190826170254_add_confirmable_to_devise.rb
@@ -3,11 +3,13 @@ class AddConfirmableToDevise < ActiveRecord::Migration[5.2]
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
     add_index :users, :confirmation_token, unique: true
     User.update_all confirmed_at: DateTime.now
   end
 
   def self.down
-    remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
+    remove_columns :users, :confirmation_token, :confirmed_at,
+                           :confirmation_sent_at, :unconfirmed_email
   end
 end
