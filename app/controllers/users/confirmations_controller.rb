0895ed41ef9cefac65570928dc24f33adb6f0b1d
oliveira-andre <desmonplays@gmail.com>
2019-08-26 17:57:20 -0400

    (new feature) finishing the redirect

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,11 +1,21 @@
 module Users
   class ConfirmationsController < Devise::ConfirmationsController
-    def show; end
+    before_action :load_user, only: :show
+
+    def show
+      @user.confirm
+      if current_user
+        redirect_to new_user_session_path
+      else
+        redirect_to root_path
+      end
+      flash[:success] = 'Email confirmado com sucesso!'
+    end
 
     private
 
-    def after_confirmation_path_for(resource_name, resource)
-      debugger
+    def load_user
+      @user = User.find_by(confirmation_token: params[:confirmation_token])
     end
   end
 end
