9fb602ba26306187da1f43fb208143b19e0ca10a
oliveira-andre <desmonplays@gmail.com>
2019-08-30 20:45:17 -0400

    (new feature) fix the if clasure

--- app/services/scheduling_reminder.rb
+++ app/services/scheduling_reminder.rb
@@ -1,17 +1,17 @@
 module SchedulingReminder
-  class << Self
+  class << self
     def execute
-      scheduling = Scheduling.where(
+      schedulings = Scheduling.where(
         status: :scheduled,
         date: ((Time.now - 2.days)..Time.now)
       )
-      scheduling.each do |scheduling|
-        if scheduling.date = (Time.now.utc - 28.hours)
-          SchedulingReminder.four_hours(scheduling).deliver_later
+      schedulings.each do |scheduling|
+        if scheduling.date == (Time.now.utc - 28.hours)
+          SchedulingReminderMailer.four_hours(scheduling).deliver_later
         end
 
-        if scheduling.date = (Time.now.utc - 6.hours)
-          SchedulingReminder.twenty_four_hours(scheduling).deliver_later
+        if scheduling.date == (Time.now.utc - 6.hours)
+          SchedulingReminderMailer.twenty_four_hours(scheduling).deliver_later
         end
       end
     end
