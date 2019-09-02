0d5424305ef7ed70cc902e46ed4ff8ec20b4084f
oliveira-andre <desmonplays@gmail.com>
2019-09-02 15:21:33 -0400

    (new feature) changing the where clauses

--- app/services/scheduling_reminder.rb
+++ app/services/scheduling_reminder.rb
@@ -1,18 +1,22 @@
 module SchedulingReminder
   class << self
     def execute
-      schedulings = Scheduling.where(
+      schedulings_twenty_four_hours = Scheduling.where(
         status: :scheduled,
-        date: ((Time.now - 2.days)..Time.now)
+        date: (Time.now.utc - 28.hours)
       )
-      schedulings.each do |scheduling|
-        if scheduling.date == (Time.now.utc - 28.hours)
-          SchedulingReminderMailer.four_hours(scheduling).deliver_later
-        end
 
-        if scheduling.date == (Time.now.utc - 6.hours)
-          SchedulingReminderMailer.twenty_four_hours(scheduling).deliver_later
-        end
+      schedulings_twenty_four_hours.each do |scheduling|
+        SchedulingReminderMailer.four_hours(scheduling).deliver_later
+      end
+
+      schedulings_four_hours = Scheduling.where(
+        status: :scheduled,
+        date: (Time.now.utc - 6.hours)
+      )
+
+      schedulings_four_hours.each do |scheduling|
+        SchedulingReminderMailer.twenty_four_hours(scheduling).deliver_later
       end
     end
   end
