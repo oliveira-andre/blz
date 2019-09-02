fdcef8c89226556062bdb1dcf86f19691772d5ca
oliveira-andre <desmonplays@gmail.com>
2019-09-02 16:45:33 -0400

    (new feature) sending filter

--- app/assets/javascripts/filter_services.js
+++ app/assets/javascripts/filter_services.js
@@ -8,9 +8,11 @@ function listeningInputs() {
 
   var categorySelect = $('select[id="filter-services-category"]');
   var dateField = $('input[id="filter-services-date"]');
-  var localTypeSelect = $('select[id="filter-services-local-type"]')
+  var localTypeSelect = $('select[id="filter-services-local-type"]');
+  var priceSelect = $('select[id="filter-services-price"]');
 
   categorySelect.on('change', sendSubmit);
+  priceSelect.on('change', sendSubmit);
   dateField.on('change', function(e) {
     var year = parseInt(dateField.val().substring(0, 4));
     if(year.toString().length == 4) {
