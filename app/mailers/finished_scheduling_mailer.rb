79ce2385ec6c6fd2b2a428004b9a5d50dcbf8e17
oliveira-andre <desmonplays@gmail.com>
2019-09-11 15:08:24 -0400

    (new feature) mailer finished scheduling

---
+++ app/mailers/finished_scheduling_mailer.rb
@@ -0,0 +1,15 @@
+class FinishedSchedulingMailer < ApplicationMailer
+  def to_establishment(scheduling)
+    @scheduling = scheduling
+
+    mail to: @scheduling.service.establishment.user.email,
+         subject: 'BLZ - O agendamento foi finalizado'
+  end
+
+  def to_user(scheduling)
+    @scheduling = scheduling
+
+    mail to: @scheduling.service.establishment.user.email,
+         subject: 'BLZ - Seu agendamento foi finalizado'
+  end
+end
