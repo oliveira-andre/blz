4e9274cf117c6a3d12d4aef0538737083ebcbd56
oliveira-andre <desmonplays@gmail.com>
2019-09-12 22:12:59 -0400

    (new feature) renaming variable

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,10 +1,10 @@
 class Click < ActiveRecord::Base
   def self.create(service_id)
-    service = $redis.get service_id
-    if service
-      $redis.incr service_id, (service + 1)
+    service_click_times = $redis.get service_id
+    if service_click_times
+      $redis.incr service_id
     else
-      $redis.incr service_id, 1
+      $redis.set service_id, 1
     end
   end
 end
