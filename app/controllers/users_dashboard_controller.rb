f800b040ae13557ad50e3a0e9cddab8b7de3d29e
oliveira-andre <desmonplays@gmail.com>
2019-09-15 10:38:17 -0400

    (new feature) filter the scheduling status

--- app/controllers/users_dashboard_controller.rb
+++ app/controllers/users_dashboard_controller.rb
@@ -1,5 +1,17 @@
 class UsersDashboardController < ApplicationController
   def index
-    @scheduling = current_user.scheduling
+    @status = params[:users_dashboard_status]
+    if @status.present?
+      scheduling_filter = current_user.scheduling.where(
+        status: @status.to_sym
+      )
+      if scheduling_filter.empty?
+        @scheduling = current_user.scheduling
+      else
+        @scheduling = scheduling_filter
+      end 
+    else
+      @scheduling = current_user.scheduling
+    end
   end
 end
