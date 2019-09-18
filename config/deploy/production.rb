1cdc80b821eeec9d7f2fc5f0425ec663d9e0e2b3
Rivelino junior <rv.fernandes.junior@gmail.com>
2019-09-18 16:48:57 -0400

    fix serve production address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :branch, 'master'
-set :server_address, 'blz.life'
+set :server_address, '35.199.118.67'
 ask(:password, nil, echo: false)
 server fetch(:server_address), user: "deploy", roles: %w{app db web} 
 set :nginx_server_name, fetch(:server_address)
