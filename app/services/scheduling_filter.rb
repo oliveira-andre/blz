f4457bb740a32c75eac7bd0d1cd197619f4208ce
oliveira-andre <desmonplays@gmail.com>
2019-09-19 23:49:38 -0400

    (new feature) creating a service to filter the schedulings gst

---
+++ app/services/scheduling_filter.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module SchedulingFilter
+  class << self
+    def execute(status, date, current_user)
+      @current_user = if current_user.establishment.present?
+                        current_user.establishment
+                      else
+                        current_user
+                      end
+      schedulings = @current_user.scheduling
+      schedulings.where!(status: status) if status.present?
+      schedulings.where!(date: apply_date_filter(date)) if date.present?
+      schedulings = @current_user.scheduling unless schedulings.present?
+      schedulings
+    end
+
+    def apply_date_filter(date)
+      t = (Time.now - 4.hours)
+      if date == 'today'
+        date_filter = "#{t.day}/#{t.month}/#{t.year}%"
+      elsif date == 'week'
+        t_week = (t - 7.days)
+        date_filter = t..t_week
+      elsif date == 'month'
+        t_month = (t - 1.month)
+        date_filter = t..t_month
+      end
+      date_filter
+    end
+  end
+end
