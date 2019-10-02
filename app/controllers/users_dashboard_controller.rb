a08c51d136400f651dca755e535faf0d128e985d
oliveira-andre <desmonplays@gmail.com>
2019-10-02 14:53:54 -0400

    (new feature) removing debugger

--- app/controllers/users_dashboard_controller.rb
+++ app/controllers/users_dashboard_controller.rb
@@ -2,6 +2,5 @@ class UsersDashboardController < ApplicationController
   def index
     @status = params[:users_dashboard_status]
     @scheduling = SchedulingFilter.execute(@status, nil, current_user)
-    debugger
   end
 end
