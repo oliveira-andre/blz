52ab549609bed694f873f11ce7ba18b3e0156170
oliveira-andre <desmonplays@gmail.com>
2019-10-16 10:49:23 -0400

    (new feature) sending mailer to contact

--- app/mailers/accepted_scheduling_mailer.rb
+++ app/mailers/accepted_scheduling_mailer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AcceptedSchedulingMailer < ApplicationMailer
   def to_establishment(scheduling)
     @scheduling = scheduling
@@ -11,5 +13,13 @@ class AcceptedSchedulingMailer < ApplicationMailer
 
     mail to: @scheduling.service.establishment.user.email,
          subject: 'BLZ - Seu agendamento foi aceito'
+  end
+
+  def to_contact(scheduling)
+    @scheduling = scheduling
+
+    mail to: 'contato@blz.life',
+         subject: "BLZ - O agendamento do
+                   #{@scheduling.service.establishment.name} foi aceito"
   end
 end
